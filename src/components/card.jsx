f6db2f0ca3bb2a3405a48a67f4edaa80086bb881
Niyal02 <bhetwalniyal@gmail.com>
2025-02-17 10:19:30 +0545

    More card components

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,8 +4,10 @@ import star from '../assets/star.png'
 
 function Card() {
   return (
-    <div className={classes.card}>
-      <img src={reactinternproj2katie} className={classes.katie} alt="" />
+    <div className={classes.card_container}>
+      <div>
+      <img src={reactinternproj2katie} className={classes.katie_img} alt="" />
+      <p className={classes.sold_out}>Sold Out</p>   {/* z index use where position absolute and reltive is used*/}
       <div className={classes.card_stats}>
         <img src={star} className={classes.card_star} alt="" />
         <span >4.8</span>
@@ -14,6 +16,79 @@ function Card() {
       </div>
       <p>Life lessons with Katie Zaferes</p>
       <p className={classes.bold}>From $125 / person</p>
+      </div>
+
+      <div>
+        <img src={reactinternproj2katie} className={classes.katie_img} alt="" />
+      <div className={classes.card_stats}>
+        <img src={star} className={classes.card_star} alt="" />
+        <span >4.8</span>
+        <span className={classes.gray}>(6) . </span>
+        <span className={classes.gray}> USA</span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p className={classes.bold}>From $125 / person</p>
+      </div>
+
+      <div>
+        <img src={reactinternproj2katie} className={classes.katie_img} alt="" />
+      <div className={classes.card_stats}>
+        <img src={star} className={classes.card_star} alt="" />
+        <span >4.8</span>
+        <span className={classes.gray}>(6) . </span>
+        <span className={classes.gray}> USA</span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p className={classes.bold}>From $125 / person</p>
+      </div>
+
+      <div>
+        <img src={reactinternproj2katie} className={classes.katie_img} alt="" />
+      <div className={classes.card_stats}>
+        <img src={star} className={classes.card_star} alt="" />
+        <span >4.8</span>
+        <span className={classes.gray}>(6) . </span>
+        <span className={classes.gray}> USA</span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p className={classes.bold}>From $125 / person</p>
+      </div>
+
+      <div>
+        <img src={reactinternproj2katie} className={classes.katie_img} alt="" />
+      <div className={classes.card_stats}>
+        <img src={star} className={classes.card_star} alt="" />
+        <span >4.8</span>
+        <span className={classes.gray}>(6) . </span>
+        <span className={classes.gray}> USA</span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p className={classes.bold}>From $125 / person</p>
+      </div>
+
+      <div>
+        <img src={reactinternproj2katie} className={classes.katie_img} alt="" />
+      <div className={classes.card_stats}>
+        <img src={star} className={classes.card_star} alt="" />
+        <span >4.8</span>
+        <span className={classes.gray}>(6) . </span>
+        <span className={classes.gray}> USA</span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p className={classes.bold}>From $125 / person</p>
+      </div>
+
+      <div>
+        <img src={reactinternproj2katie} className={classes.katie_img} alt="" />
+      <div className={classes.card_stats}>
+        <img src={star} className={classes.card_star} alt="" />
+        <span >4.8</span>
+        <span className={classes.gray}>(6) . </span>
+        <span className={classes.gray}> USA</span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p className={classes.bold}>From $125 / person</p>
+      </div>
     </div>
   )
 }
